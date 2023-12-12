f6a5857de544dffec942bf7668f4ef3af181bbc6
= <=>
2023-12-12 22:21:05 +0700

    (fix) Fixed .env.example and config for mongoose

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,11 @@
+import * as dotenv from "dotenv";
+dotenv.config();
 import mongoose from "mongoose";
 
 export default class Database {
 	public async connect() {
 		try {
-			await mongoose.connect("mongodb://localhost:27017/blog");
+			await mongoose.connect(String(process.env.MONGO_URI));
 			console.log("connected to the mongoDB");
 		} catch (err) {
 			console.log("Connect failure");
