dc6d3c8b6fb618acef3311455733e653f42e6dda
= <patohru2006@gmail.com>
2023-12-22 15:28:44 +0700

    (fix) delete dotenv

--- src/app/controllers/AuthController.ts
+++ src/app/controllers/AuthController.ts
@@ -1,5 +1,5 @@
-import * as dotenv from "dotenv";
-dotenv.config();
+// import * as dotenv from "dotenv";
+// dotenv.config();
 import { NextFunction, Request, Response } from "express";
 import UserModel from "../models/User";
 import bcrypt from "bcryptjs";
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-require("dotenv").config();
 import express, { Express, Request, Response } from "express";
 import morgan from "morgan";
 import path from "path";
